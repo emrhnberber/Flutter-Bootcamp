2e76bf4d220d323b7e686ccc66d35dbb480a7284
Emirhan Berber <emirhanberber2018@gmail.com>
2024-04-30 15:45:41 +0300

    İkinci ödev tamamlandı.
    Ödev 2 tamamlandı.

--- learningdart/lib/odevler/ödev2.dart
+++ learningdart/lib/odevler/ödev2.dart
@@ -3,5 +3,9 @@ import 'package:learningdart/odevler/%C3%B6dev2_main.dart';
 void main(){
   var emirhan = Odev2();
 
+  // Ödev 2 Bölüm 2 Mesai saatine göre maaş hesaplama;
+  emirhan.maashesapla(160);
 
+  //Ödev 2 BÖLÜM 3 Otopark ücreti hesaplama;
+  emirhan.otoparkucret(0);
 }
--- learningdart/lib/odevler/ödev2_main.dart
+++ learningdart/lib/odevler/ödev2_main.dart
@@ -1,7 +1,34 @@
 class Odev2{
-  //2
-  int maashesapla(int kacSaatCalisti, int ){
+  //ÖDEV 2 BÖLÜM 2
+  int maashesapla(int kacSaatCalisti){
+    int toplam;
+    int ekMesaiSaat;
 
-    return kacSaatCalisti;
+    if(kacSaatCalisti > 150){
+      ekMesaiSaat = kacSaatCalisti - 150;
+      toplam = kacSaatCalisti * 40 + ekMesaiSaat * 40;
+    }
+    else{
+      toplam = kacSaatCalisti *40;
+    }
+    print("Bu ay ki maaşınız: $toplam TL.");
+    return toplam;
+  }
+
+  //ÖDEV 2 BÖLÜM 3
+
+  int otoparkucret(int kacSatKaldi){
+    int toplam;
+    int dusukucret;
+
+    if(kacSatKaldi <= 1){
+      toplam =50;
+    }
+    else{
+      dusukucret = kacSatKaldi - 1;
+      toplam = dusukucret*10+50;
+    }
+    print("Ödenecek Tutar: $toplam TL");
+    return toplam;
   }
 }
